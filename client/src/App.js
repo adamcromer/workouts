5069d315b843d1decf81d032a6df850a6c5b6808
adamjorycromer <adamjorycromer@gmail.com>
2019-04-08 21:48:54 -0600

    added body and updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Nav from "./components/Nav";
+import Body from "./components/Body";
 import './App.css';
 
 class App extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <Body
+          currentPage={this.state.currentPage}
+        />
       </div>
     );
   }
