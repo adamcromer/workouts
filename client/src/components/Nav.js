2b8df73858d52f860c08b61f946af26098cec3d8
fkakatie <sextonskatie@gmail.com>
2019-04-10 00:02:56 -0600

    nav bar working ...mostly

---
+++ client/src/components/Nav.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+import LoginButtons from './LoginButtons';
+import UserInfo from './UserInfo';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+class Nav extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuth: false
+    }
+}
+
+  // updateWidth = () => {
+  //   const newState = { width: window.innerWidth };
+
+  //   if (this.state.open && newState.width > 991) {
+  //     newState.open = false;
+  //   }
+
+  //   this.setState(newState);
+  // };
+
+  // toggleNav = () => {
+  //   this.setState({ open: !this.state.open });
+  // };
+
+  // componentDidMount() {
+  //   window.addEventListener("resize", this.updateWidth);
+  // }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener("resize", this.updateWidth);
+  // }
+
+  render() {
+    return (
+
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Workouts</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <div className="mr-auto">
+            { this.state.isAuth ? 
+            <UserInfo /> : <LoginButtons /> }
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
+
+    )
+  }
+}
+
+export default Nav;
