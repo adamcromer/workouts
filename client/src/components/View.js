2ecea8c3c0b5fa39e2d8f585d1189828bf79ebf1
Zach Timmerman <zachtimmerman@Zachs-MacBook-Pro.local>
2019-04-10 21:41:45 -0600

    added pages, updated components, loads locally

---
+++ client/src/components/View.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class View extends React.Component {
+
+
+    render() {
+        return (
+
+            <div className = "view">
+                <h1>View</h1>
+            </div>
+        );
+    }
+}
+export default View;
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 
 class About extends React.Component {
 
