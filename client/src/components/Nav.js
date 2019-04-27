5f342f9f571be84ef5f5217cea1ac164bab19810
adamjorycromer <adamjorycromer@gmail.com>
2019-04-27 08:42:31 -0600

    fixed nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -20,9 +20,9 @@ class Nav extends Component {
       <Navbar bg="dark" variant="dark" expand="lg" fixed="stick">
         <Container className="justify-content-center">
           <Navbar.Brand className = "brand-name">
-          <a className = "navbar-brand" href="/">Workins
+          <a className = "navbar-brand" href="/">
           <img className="nav-logo" alt="Workout Logo" src="/images/Workout-Favicon-Light.png" style={{width:55, marginTop: -5}} />
-          Workouts
+          Workins
           </a>
           </Navbar.Brand>
           {/* <div>
